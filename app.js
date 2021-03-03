e8ddbc3f98cf348576c71340b09519661ee6c91a
hftamayo <hftamayo@gmail.com>
2021-03-03 14:31:48 -0600

    request and redirecting

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ const server = http.createServer((req, res) => {
     //las siguientes propiedades ayudan a comprender el request del usuario
     //probar con localhost:3005, localhost:3005/test
     console.log(req.url, req.method, req.headers);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My first page</title></head>');
+    res.write('<body><h1>Hello from Node.Js Server!</h1></body>');
+    res.write('</html>');
+    res.end();
 }
 );
 //ejecuta el proceso principal, por default en produccion buscara ejecutarse en el puerto 80
