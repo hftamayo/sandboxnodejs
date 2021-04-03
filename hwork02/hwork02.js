1aeedd83fccf9d7f617344bc448ebf337f8f23c0
hftamayo <hftamayo@gmail.com>
2021-04-02 19:40:42 -0600

    hwork02: beta version

--- hwork02/hwork02.js
+++ hwork02/hwork02.js
@@ -3,17 +3,31 @@
 const express = require('express');
 
 const app = express();
-
+/*
 app.use('/', (req, res, next) => {
     console.log("This is a message from the main middleware route!");
     next();
 });
 
-//primer midleware
+//segundo midleware
+app.use('/users', (req, res, next) => {
+    console.log('this is a message from users middleware route');
+    res.send('<hl>Add User page</h1>');
+});
+*/
+
+//approach based on 3rd requirement
+//si pongo de segundo a /users, nunca se va a mostrar
+//esto se llama "filters"
 app.use('/users', (req, res, next) => {
     console.log('this is a message from users middleware route');
     res.send('<hl>Add User page</h1>');
 });
 
+//segundo midleware
+app.use('/', (req, res, next) => {
+    console.log("This is a message from the main middleware route!");
+    res.send('<hl>Welcome to the root page</h1>');
+});
 
 app.listen(3005)
