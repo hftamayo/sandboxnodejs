dd53b38f1f3650e81d5bb175c2207d39da891aa6
hftamayo <hftamayo@gmail.com>
2021-04-13 15:29:14 -0600

    serving html pages from routes

--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,17 @@
+/*
+la libreria path ayuda a construir las referencias absolutas
+hacia los scripts js, caso contrario, si nosotros ponemos './views/shop.html'
+node.js buscará en el sistema operativo esta ubicacion partiendo de /
+*/
+const path= require('path');
 const express = require('express');
+const { schedulingPolicy } = require('cluster');
 
 const router = express.Router();
 
 //segundo midleware
 router.get('/', (req, res, next) => {
-    //una vez que ya no tenemos otro mware, enviamos a response
-    res.send('<hl>Hello from Express</h1>');
+    res.sendFile(path.join(__dirname,'../', 'views', 'shop.html'));
 });
 
 module.exports = router;
