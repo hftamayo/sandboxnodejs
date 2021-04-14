f8b5931f7e0ff8bb4ad9df495cf79ed0f86a03a8
hftamayo <hftamayo@gmail.com>
2021-04-14 12:24:12 -0600

    routing an error page

--- routes/admin2.js
+++ routes/admin2.js
@@ -6,7 +6,7 @@ const router = express.Router();
 //admin/add-product -> via GET method
 router.get('/add-product', (req, res, next) => {
     //res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>');
-    res.sendFile(path.join(__dirname,'../', 'views', 'add-product.html'));    
+    res.sendFile(path.join(__dirname,'..', 'views', 'add-product.html'));    
 });
 
 //admin/add-product -> via POST method
