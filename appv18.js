8bacf59d19b7cf916f083f9b38cdc0e3e8afaddb
hftamayo <hftamayo@gmail.com>
2021-05-03 19:53:33 -0600

    serving files statically

---
+++ appv18.js
@@ -0,0 +1,20 @@
+//leccion 71 y 73 : returning 404 error page
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const adminRoutes = require('./routes/admin2');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+//serving main.css statically
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/admin', adminRoutes);//solo las rutas que inicien con /admin se redirigen a adminRoutes
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', 'error404.html'));
+});
+
+app.listen(3005)
