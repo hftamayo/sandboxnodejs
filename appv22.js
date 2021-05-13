ebe8dcd4021289cb8c4002bb7a71c703b85812cd
hftamayo <hftamayo@gmail.com>
2021-05-13 15:06:13 -0600

    lesson 84: translate html to pug

---
+++ appv22.js
@@ -0,0 +1,25 @@
+//leccion 81 : installing and implementing pug template
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+//definicion de variables globales
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+
+const adminData = require('./routes/admin4');
+const shopRoutes = require('./routes/shopv3');
+app.use(bodyParser.urlencoded({extended: false}));
+//serving main.css statically
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/admin', adminData.routes);//solo las rutas que inicien con /admin se redirigen a adminRoutes
+app.use(shopRoutes);
+
+
+app.use((req, res, next) => {
+    res.status(404).render('error404');
+});
+
+app.listen(3005)
