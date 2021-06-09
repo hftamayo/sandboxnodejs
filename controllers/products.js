2e50f54d75e4fdb5530b4bef0e1ed2e606d40556
hftamayo <hftamayo@gmail.com>
2021-06-09 10:11:24 -0600

    adding model class

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,5 @@
-const products = [];
+//para referenciar clases se usa letra mayuscula
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
     res.render('add-product',
@@ -12,11 +13,13 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-    products.push({ title: req.body.title });
+    const product = new Product(req.body.title);
+    product.save();
     res.redirect('/');
 }
 
 exports.getProducts = (req, res, next) => {
+    const products = Product.fetchAll();
     res.render('shop', {
         prods: products,
         pageTitle: 'Shop Best products',
