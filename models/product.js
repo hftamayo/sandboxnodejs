705aa19d5f30c72a6a84907784b9be9b90b6f500
hftamayo <hftamayo@gmail.com>
2021-06-09 10:57:04 -0600

    storing data in json file part1

--- models/product.js
+++ models/product.js
@@ -1,16 +1,45 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+
 
 module.exports = class Product {
-    constructor(t){
+    constructor(t) {
         this.title = t;
     }
 
-    save(){
-        products.push(this);
+    save() {
+        //data es la carpeta donde estara el json
+        const p = path.
+            join(path.dirname(process.mainModule.filename),
+                'data', 'products.json');
+        fs.readFile(p, (err, fileContent) => {
+            //in case the file doesnt not exist
+            //it needs to be created
+            let products = [];
+            if (!err) {
+                products = JSON.parse(fileContent);
+            }
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
     }
 
-    static fetchAll(){
-        return products;
-    }
+    /* metodo sincrono
+    sin el callback genera un error
+    */
 
-}
+    static fetchAll(cb) {
+        //p se va a refactorar mas adelante
+        const p = path.
+            join(path.dirname(process.mainModule.filename),
+                'data', 'products.json');
+        fs.readFile(p, (err, fileContent) => {
+            if (err) {
+                cb([]);
+            }
+            cb(JSON.parse(fileContent));
+        });
+    }
+};
