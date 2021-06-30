26324dd182505d6735955eedfef27f4f2df0894a
hftamayo <hftamayo@gmail.com>
2021-06-30 16:51:24 -0600

    deleting a cart item

--- controllers/admin.js
+++ controllers/admin.js
@@ -65,6 +65,12 @@ exports.getProducts = (req, res, next) => {
             pageTitle: 'Shop right here',
             path: '/admin/products'
         });
-    });        
+    });    
+};
+    
+    exports.postDeleteProduct = (req, res, next) => {
+        const prodId = req.body.productId;
+        Product.deleteById(prodId);
+        res.redirect('/admin/products');
 };
 
