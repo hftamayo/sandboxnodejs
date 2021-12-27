6db6384f76cea0fbde9cccdc49a3f9de0636bf14
hftamayo <hftamayo@gmail.com>
2021-12-27 13:18:16 -0600

    cleaning up the project before apply mongoose

--- appv24.js
+++ appv24.js
@@ -2,9 +2,9 @@
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const errorController = require("./controllers/error");
-const mongoConnect = require("./util/database").mongoConnect;
 const User = require("./models/user");
 
 const app = express();
@@ -13,17 +13,14 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-
 
 app.use(bodyParser.urlencoded({ extended: false }));
 //serving main.css statically
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-
   User.findById("61267055e959b8480f230d12")
     .then((user) => {
       req.user = new User(user.name, user.email, user.cart, user._id);
@@ -32,15 +29,15 @@ app.use((req, res, next) => {
     .catch((err) => console.log(err));
 });
 
-
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
+mongoose.connect(
+  "mongodb+srv://node:Node123$@nodecluster.hfnls.mongodb.net/shop?retryWrites=true&w=majority"
+).then(result => {
   app.listen(3005);
-
-
+}).catch(err => {
+  console.log(err);
 });
-
