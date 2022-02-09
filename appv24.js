efc8e5b60cf1973ef0fa0ea5c52d6cdedb1b4c9c
hftamayo <hftamayo@gmail.com>
2022-02-08 19:53:15 -0600

    setting up session package and enviro

--- appv24.js
+++ appv24.js
@@ -3,6 +3,7 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const session = require("express-session");
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
@@ -20,6 +21,9 @@ const authRoutes = require("./routes/auth");
 app.use(bodyParser.urlencoded({ extended: false }));
 //serving main.css statically
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({ secret: "virtualstore", resave: false, saveUninitialized: false })
+);
 
 app.use((req, res, next) => {
   User.findById("61df85611285555f2605e931")
