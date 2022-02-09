5c6a33bb40075e660bfcc4f2e58ef97e6b440b1f
hftamayo <hftamayo@gmail.com>
2022-02-08 19:59:16 -0600

    adding session to midleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,22 @@
 exports.getLogin = (req, res, next) => {
   //si la cookie no existe, esta variable da error
+  /*
   const isLoggedIn = req
   .get("Cookie")
   .split(";")[1]
   .trim()
   .split("=")[1] === 'true';
+  */
+ console.log(req.session.isLoggedIn);
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated: isLoggedIn,
+    isAuthenticated: false,
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader("Set-Cookie", "loggedIn=true; HttpOnly"); //HttpOnly = previene XSS
+  //gestion por cookies: res.setHeader("Set-Cookie", "loggedIn=true; HttpOnly"); //HttpOnly = previene XSS
+  req.session.isLoggedIn = true;
   res.redirect("/");
 };
