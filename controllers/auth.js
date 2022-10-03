e735c475948d2fa9d56f1b888957f0cbfb748d7c
hftamayo <hftamayo@gmail.com>
2022-10-03 15:54:21 -0600

    business logic for generate a token for reset

--- controllers/auth.js
+++ controllers/auth.js
@@ -150,11 +150,38 @@ exports.getReset = (req, res, next) => {
 
 exports.postReset = (req, res, next) => {
   crypto.randomBytes(32, (err, buffer) => {
-    if(err){
+    if (err) {
       console.log(err);
-      return res.redirect('/reset');
+      return res.redirect("/reset");
     }
-    const token = buffer.toString('hex');
-  });
+    const token = buffer.toString("hex");
+    User.findOne({ email: req.body.email })
+      .then((user) => {
+        if (!user) {
+          req.flash("error", "No account with that email found");
+          return res.redirect("/reset");
+        }
+        user.resetToken = token;
+        user.resetTokenExpiration = Date.now() + 3600000;
+        return user.save();
+      })
+      .then((result) => {
+        res.redirect('/');
+        /*codigo spaguetti: ponerlo en un metodo */
+        transporter.sendMail({
+          to: req.body.email,
+          from: "users@letsorder.com",
+          subject: "Password reset",
+          html: `
+          <p>You requested a password reset</p>
+          <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password</p>
 
+          
+          `,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 };
--- models/user.js
+++ models/user.js
@@ -18,6 +18,8 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
+  resetToken: String,
+  resetTokenExpiration: Date,
   cart: {
     items: [
       {
